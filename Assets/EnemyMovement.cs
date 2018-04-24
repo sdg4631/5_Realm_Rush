8174ead642fb7d9a362b871dec07e02908336bae
S&K <sdg4631@gmail.com>
2018-04-24 13:43:21 +0900

    C# Lists for Custom Path

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour 
+{
+
+	[SerializeField] List<Block> path;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PrintAllWaypoints();
+	}
+
+    // Update is called once per frame
+    void Update () 
+	{
+
+	}
+
+	private void PrintAllWaypoints()
+    {
+        		foreach(Block waypoint in path)
+		{
+			print (waypoint.name);
+		}
+    }
+}
