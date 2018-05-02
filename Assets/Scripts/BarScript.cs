e83e22c4a3d8ed090ad1a85fdf91368a4071152b
S&K <sdg4631@gmail.com>
2018-05-02 22:35:32 +0900

    Started work on health bar

---
+++ Assets/Scripts/BarScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour 
+{
+	[SerializeField] private float fillAmount;
+	[SerializeField] private Image content; 
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		HandleBar();
+	}
+
+	private void HandleBar()
+	{
+		content.fillAmount = fillAmount;
+	}
+
+	private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+	{
+		return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	}
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour 
+{
+	[SerializeField] int playerHitPoints = 10;
+	[SerializeField] ParticleSystem baseExplosionParticlePrefab;
+
+	void OnTriggerEnter()
+	{
+		playerHitPoints--;
+		if(playerHitPoints <= 0)
+		{
+			BaseExplosion();
+		}
+	}
+
+	void BaseExplosion()
+	{
+		var baseExplosionFX = Instantiate(baseExplosionParticlePrefab, transform.position, Quaternion.identity);
+		baseExplosionFX.Play();
+		float destroyDelay = 5f;
+		Destroy(baseExplosionParticlePrefab.gameObject, destroyDelay);
+		Destroy(gameObject);
+	}
+
+}
